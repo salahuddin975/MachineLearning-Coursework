e70be2c17b69f8d684347b6e810936139cca28f8
Salah Uddin <salahuddin975@gmail.com>
2019-11-11 23:36:37 -0600

    add report.

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,20 +1,15 @@
 import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import BaggingClassifier
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import cross_val_predict
-from sklearn.model_selection import KFold
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import cross_validate
-from sklearn.metrics import recall_score
+from sklearn.model_selection import validation_curve
+from sklearn.model_selection import learning_curve
 
 
 def plot_comparison_without_k_fold(d_tree_errors, svm_errors, sgd_errors):
@@ -63,6 +58,8 @@ def test_train_error_vs_k_fold(bagging_d_tree_train_errors, bagging_svm_train_er
     ax.legend(loc = 4, fancybox=True, framealpha=0.5)
 
     plt.title("Comparisons among different estimators for Bagging and Boosting")
+    plt.xlabel("K-fold")
+    plt.ylabel("Error rate")
     plt.xticks(x)
     plt.show()
 
@@ -81,6 +78,8 @@ def bagging_test_train_error_vs_k_fold(bagging_d_tree_train_errors, bagging_svm_
     ax.legend(loc = 4, fancybox=True, framealpha=0.5)
 
     plt.title("Comparisons among different estimators using Bagging")
+    plt.xlabel("K-fold")
+    plt.ylabel("Error rate")
     plt.xticks(x)
     plt.show()
 
@@ -99,6 +98,8 @@ def boosting_test_train_error_vs_k_fold(boosting_d_tree_train_errors, boosting_s
     ax.plot(x, boosting_sgd_test_errors, label="sgd test errors")
 
     ax.legend(loc = 4, fancybox=True, framealpha=0.5)
+    plt.xlabel("K-fold")
+    plt.ylabel("Error rate")
     plt.xticks(x)
     plt.title("Comparisons among different estimators using Boosting")
     plt.show()
@@ -114,6 +115,8 @@ def d_tree_test_train_error_vs_k_fold(bagging_d_tree_train_errors, bagging_d_tre
     plt.plot(x, boosting_d_tree_test_errors, label="Boosting d-tree test errors")
 
     plt.title("Bagging Boosting comparison using d-tree")
+    plt.xlabel("K-fold")
+    plt.ylabel("Error rate")
     plt.xticks(x)
     plt.legend(loc = 4, fancybox=True, framealpha=0.5)
     plt.show()
@@ -132,6 +135,8 @@ def svm_test_train_error_vs_k_fold(bagging_svm_train_errors, bagging_svm_test_er
     ax.legend(loc = 4, fancybox=True, framealpha=0.5)
 
     plt.title("Bagging Boosting comparison using SVM")
+    plt.xlabel("K-fold")
+    plt.ylabel("Error rate")
     plt.xticks(x)
     plt.show()
 
@@ -149,8 +154,52 @@ def sgd_test_train_error_vs_k_fold(bagging_sgd_train_errors, bagging_sgd_test_er
 
     ax.legend(loc = 4, fancybox=True, framealpha=0.5)
     plt.xticks(x)
+    plt.xlabel("K-fold")
+    plt.ylabel("Error rate")
     plt.title("Bagging Boosting comparison using SGD")
     plt.show()
+
+
+def plot_test_train_error_vs_complexity(train_error, test_error):
+    x = [1, 2, 3, 4, 5, 6, 7, 8]
+    plt.plot(x, train_error, label="train_error")
+    plt.plot(x, test_error, label="test_error")
+
+    plt.legend(loc = 1, fancybox=True, framealpha=0.5)
+    plt.xticks(x)
+    plt.xlabel("Testing Fold No.")
+    plt.ylabel("Error rate")
+    plt.title("Graph for D-tree(Bagging)")
+    plt.show()
+
+
+def plot_tree_depth_vs_complexity(bagging_train_errors, bagging_test_errors, boosting_train_errors, boosting_test_errors):
+    x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+#    plt.plot(x, bagging_train_errors, label="bagging train error")
+#    plt.plot(x, bagging_test_errors, label="bagging test error")
+    plt.plot(x, boosting_train_errors, label="boosting train error")
+    plt.plot(x, boosting_test_errors, label="boosting test error")
+
+    plt.legend(loc = 1, fancybox=True, framealpha=0.5)
+    plt.xticks(x)
+    plt.xlabel("Depth of the tree")
+    plt.ylabel("Error rate")
+    plt.title("D-tree depth vs train and test error")
+    plt.show()
+
+
+def plot_complexity_number_of_trees(train_error, test_error):
+    x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    plt.plot(x, train_error, label="train_error")
+    plt.plot(x, test_error, label="test_error")
+
+    plt.legend(loc = 1, fancybox=True, framealpha=0.5)
+    plt.xticks(x)
+    plt.xlabel("Number of bags")
+    plt.ylabel("Error rate")
+    plt.title("D-tree number of bags vs train and test error(Bagging)")
+    plt.show()
+
 
 
 def run_bagging_model(estimator):
@@ -187,7 +236,8 @@ def run_model_only():
     plot_comparison_without_k_fold(d_tree_errors, svm_errors, sgd_errors)
 
 
-def run_bagging_with_cross_validation(estimator, k_fold):
+def run_bagging_with_cross_validation(estimator, k_fold, num_trees):
+    print(num_trees)
     clf = BaggingClassifier(base_estimator=estimator, n_estimators=num_trees)
     scores = cross_validate(clf, ionosphere_data, ionosphere_target, return_train_score = True, cv=k_fold)
     train_error = 1 - scores['train_score'].mean()
@@ -196,7 +246,8 @@ def run_bagging_with_cross_validation(estimator, k_fold):
     return train_error, test_error
 
 
-def run_boosting_with_cross_validation(estimator, k_fold):
+def run_boosting_with_cross_validation(estimator, k_fold, num_trees):
+    print(num_trees)
     clf = AdaBoostClassifier(base_estimator=estimator, n_estimators=num_trees, algorithm='SAMME')
     scores = cross_validate(clf, ionosphere_data, ionosphere_target, return_train_score = True, cv=k_fold)
     train_error = 1 - scores['train_score'].mean()
@@ -206,6 +257,7 @@ def run_boosting_with_cross_validation(estimator, k_fold):
 
 
 def run_model_using_kfold():
+    num_trees = 50
     d_tree = DecisionTreeClassifier()
     svm = SVC(probability=True, kernel='linear')
     sgd = SGDClassifier(loss='hinge')
@@ -227,27 +279,27 @@ def run_model_using_kfold():
 
     for i in range(5):
         k_fold = k_fold + 2
-        scores = run_bagging_with_cross_validation(d_tree, k_fold)
+        scores = run_bagging_with_cross_validation(d_tree, k_fold, num_trees)
         bagging_d_tree_train_errors.append(scores[0])
         bagging_d_tree_test_errors.append(scores[1])
 
-        scores = run_bagging_with_cross_validation(svm, k_fold)
+        scores = run_bagging_with_cross_validation(svm, k_fold, num_trees)
         bagging_svm_train_errors.append(scores[0])
         bagging_svm_test_errors.append(scores[1])
 
-        scores = run_bagging_with_cross_validation(sgd, k_fold)
+        scores = run_bagging_with_cross_validation(sgd, k_fold, num_trees)
         bagging_sgd_train_errors.append(scores[0])
         bagging_sgd_test_errors.append(scores[1])
 
-        scores = run_boosting_with_cross_validation(d_tree, k_fold)
+        scores = run_boosting_with_cross_validation(d_tree, k_fold, num_trees)
         boosting_d_tree_train_errors.append(scores[0])
         boosting_d_tree_test_errors.append(scores[1])
 
-        scores = run_boosting_with_cross_validation(svm, k_fold)
+        scores = run_boosting_with_cross_validation(svm, k_fold, num_trees)
         boosting_svm_train_errors.append(scores[0])
         boosting_svm_test_errors.append(scores[1])
 
-        scores = run_boosting_with_cross_validation(sgd, k_fold)
+        scores = run_boosting_with_cross_validation(sgd, k_fold, num_trees)
         boosting_sgd_train_errors.append(scores[0])
         boosting_sgd_test_errors.append(scores[1])
 
@@ -272,17 +324,92 @@ def run_model_using_kfold():
                                    boosting_sgd_train_errors, boosting_sgd_test_errors )
 
 
+def get_train_and_test_error_bagging(estimator, k_fold):
+    clf = BaggingClassifier(base_estimator=estimator, n_estimators=num_trees)
+    scores = cross_validate(clf, ionosphere_data, ionosphere_target, return_train_score=True, cv=k_fold)
+    train_errors = 1 - scores['train_score']
+    test_errors = 1 - scores['test_score']
+    return train_errors, test_errors
+
+
+def get_train_and_test_error_boosting(estimator, k_fold):
+    clf = BaggingClassifier(base_estimator=estimator, n_estimators=num_trees)
+    scores = cross_validate(clf, ionosphere_data, ionosphere_target, return_train_score=True, cv=k_fold)
+    train_errors = 1 - scores['train_score']
+    test_errors = 1 - scores['test_score']
+    return train_errors, test_errors
+
+
+def find_complexity():
+    k_fold = 8
+
+    d_tree = DecisionTreeClassifier()
+    svm = SVC(probability=True, kernel='linear')
+    sgd = SGDClassifier(loss='hinge')
+
+    train_errors, test_errors = get_train_and_test_error_bagging(d_tree, k_fold)
+    plot_test_train_error_vs_complexity(train_errors, test_errors)
+
+#    train_errors, test_errors = get_train_and_test_error_boosting(svm, k_fold)
+#    plot_test_train_error_vs_complexity(train_errors, test_errors)
+
+
+def find_complexity_with_d_tree_depth():
+    k_fold = 8
+    num_trees = 20
+    bagging_train_errors = []
+    bagging_test_errors = []
+    boosting_train_errors = []
+    boosting_test_errors = []
+
+    for i in range(1, 13):
+        print(i)
+        d_tree = DecisionTreeClassifier(max_depth=i)
+        bagging_scores = run_bagging_with_cross_validation(d_tree, k_fold, num_trees)
+        boosting_scores = run_boosting_with_cross_validation(d_tree, k_fold, num_trees)
+        print(bagging_scores[0], bagging_scores[1])
+        bagging_train_errors.append(bagging_scores[0])
+        bagging_test_errors.append(bagging_scores[1])
+        boosting_train_errors.append(boosting_scores[0])
+        boosting_test_errors.append(boosting_scores[1])
+
+    plot_tree_depth_vs_complexity(bagging_train_errors, bagging_test_errors, boosting_train_errors, boosting_test_errors)
+
+
+
+def find_complexity_with_number_of_bags():
+    k_fold = 8
+    train_errors = []
+    test_errors = []
+    d_tree = DecisionTreeClassifier(max_depth=6)
+    svm = SVC(probability=True, kernel='linear')
+    sgd = SGDClassifier(loss='hinge')
+
+    num_trees = 0
+    for i in range(15):
+        num_trees = num_trees + 1
+        scores = run_bagging_with_cross_validation(d_tree, k_fold, num_trees)
+#        scores = run_boosting_with_cross_validation(d_tree, k_fold, num_trees)
+        print(scores[0], scores[1])
+        train_errors.append(scores[0])
+        test_errors.append(scores[1])
+
+    plot_complexity_number_of_trees(train_errors, test_errors)
+
+
 if __name__ == '__main__':
-    dataset = pd.read_csv('ionosphere.csv')
-#    print(dataset)
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/ionosphere/ionosphere.data"
+    dataset = pd.read_csv(url, delimiter=',')
 
     arr_val = dataset.values
     ionosphere_data = arr_val[:, 0:34]
     ionosphere_target = arr_val[:, 34]
     x_train, x_test, y_train, y_test = train_test_split(ionosphere_data, ionosphere_target, test_size=0.3, random_state=0)
 
-    num_trees = 100
+    num_trees = 50
 
-#    run_model_only()
+    run_model_only()
     run_model_using_kfold()
-
+    find_complexity()
+    find_complexity_with_d_tree_depth()
+    find_complexity_with_number_of_bags()
